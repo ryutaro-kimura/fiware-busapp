de377f03531a0706d9afa6409277c0af0d9a5be5
ryutaro-kimura <sasuker2264@gmail.com>
2022-11-21 15:25:29 +0900

    add,wip: fiwareにリクエストを送る記述を追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import { getGithubId } from "../src/utils/api";
 export default function Home() {
   const [busEntity, setBusEntity] = useState<number>();
   useEffect(() => {
+    // getOrionData;
     getGithubId().then(id => {
       setBusEntity(id);
     });
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,57 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import { BASE_API_URL } from "../../secrets";
+import AmazonCognitoIdentity, {
+  IAuthenticationDetailsData,
+  ICognitoUserPoolData
+} from "amazon-cognito-identity-js";
+
+const authenticationData: IAuthenticationDetailsData = {
+  Username: process.env.NEXT_PUBLIC_USERNAME!,
+  Password: process.env.NEXT_PUBLIC_PASSWORD
+};
+const authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(
+  authenticationData
+);
+// const poolData: ICognitoUserPoolData = {
+//   UserPoolId: process.env.NEXT_PUBLIC_USER_POOL_ID,
+//   ClientId: process.env.NEXT_PUBLIC_APP_CLIENT_ID
+// };
+// const userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+// const userData = {
+//   Username: authenticationData.Username,
+//   Pool: userPool
+// };
+// const cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+
+// export const getOrionData = cognitoUser.authenticateUser(
+//   authenticationDetails,
+//   {
+//     onSuccess: async function hoge(result) {
+//       const accessToken = result.getAccessToken().getJwtToken();
+
+//       const idToken = result.getIdToken().getJwtToken();
+//       console.log(`TOKEN: ${idToken}`);
+
+//       const resp = await fetch(
+//         `${process.env.NEXT_PUBLIC_ORION_ENDPOINT}/v2/entities?type=Room`,
+//         {
+//           headers: {
+//             Authorization: idToken
+//           }
+//         }
+//       );
+//       const body = await resp.json();
+//       return body;
+//       console.log(body);
+//     },
+
+//     onFailure: function (err) {
+//       console.log(`Got Error =======`);
+//       console.log(err);
+//     }
+//   }
+// );
 
 // export const apiORION = async () => {
 //   const response = await fetch(BASE_API_URL, {});
@@ -13,11 +64,12 @@ import { BASE_API_URL } from "../../secrets";
 //   return await response.json();
 // };
 
-const url = "https://api.github.com/users/ryutaro-kimura";
-
+// ↑ほぼコピペ-----
 export const getGithubId = async () => {
+  const url = "https://api.github.com/users/ryutaro-kimura";
   const data = await fetch(url);
-  console.log(data);
+  console.log(authenticationData);
+
   const json = await data.json();
   const githubId: number = json.login;
   return await githubId;
