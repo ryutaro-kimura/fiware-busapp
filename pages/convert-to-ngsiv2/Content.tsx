44b3669a4d5f64931f2228c8700e945c1c961b4a
ryutaro-kimura <sasuker2264@gmail.com>
2022-12-15 05:22:26 +0900

    add: コピペでファイル添付UIの作成

--- pages/convert-to-ngsiv2/Content.tsx
+++ pages/convert-to-ngsiv2/Content.tsx
@@ -1,9 +1,20 @@
 export const ConvertToNGSIv2Content: React.FC = () => {
   return (
     <div>
-      <h1 className='text-3xl font-bold text-red-400 bg-black underline'>
-        Hello world!
-      </h1>
+      <label
+        className='block mb-2 text-sm font-medium text-gray-900'
+        htmlFor='file_input'>
+        Upload file
+      </label>
+      <input
+        className='block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none'
+        aria-describedby='file_input_help'
+        id='file_input'
+        type='file'
+      />
+      <p className='mt-1 text-sm text-gray-500'>
+        SVG, PNG, JPG or GIF (MAX. 800x400px).
+      </p>
     </div>
   );
 };
