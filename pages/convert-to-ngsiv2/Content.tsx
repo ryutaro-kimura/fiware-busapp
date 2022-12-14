f32a0ae9debd87672ff71784cfbb2226c7a9f313
ryutaro-kimura <sasuker2264@gmail.com>
2022-12-15 04:50:42 +0900

    add: pageExtensionsを導入

---
+++ pages/convert-to-ngsiv2/Content.tsx
@@ -0,0 +1,7 @@
+export const ConvertToNGSIv2Content: React.FC = () => {
+  return (
+    <div>
+      <div>hoge</div>
+    </div>
+  );
+};
---
+++ pages/convert-to-ngsiv2/index.page.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from "next";
+
+import { ConvertToNGSIv2Content } from "./Content";
+
+const ConvertToNGSIv2Page: NextPage = () => {
+  return <ConvertToNGSIv2Content />;
+};
+
+export default ConvertToNGSIv2Page;
