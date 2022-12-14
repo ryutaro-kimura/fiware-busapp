26ec10096f9f0c1436da750977d4cf86335da1f7
ryutaro-kimura <sasuker2264@gmail.com>
2022-12-15 07:07:16 +0900

    add: csvからjsonへの変換機能の追加

--- pages/convert-to-ngsiv2/Content.tsx
+++ pages/convert-to-ngsiv2/Content.tsx
@@ -1,4 +1,30 @@
+import { parse } from "papaparse";
+import { useForm } from "react-hook-form";
+
+type Inputs = {
+  file: File[];
+};
+
 export const ConvertToNGSIv2Content: React.FC = () => {
+  const { register, handleSubmit } = useForm<Inputs>();
+  const onSubmit = async (data: Inputs): Promise<void> => {
+    const json = await csvToJson(data.file[0]);
+    console.log(json);
+  };
+
+  const csvToJson = async (file: File) => {
+    return new Promise((resolve, reject) => {
+      parse(file, {
+        complete: results => {
+          resolve(results?.data);
+        },
+        error: () => {
+          reject(new Error("csv parse err"));
+        }
+      });
+    });
+  };
+
   return (
     <div className='px-4'>
       <div className='mt-6 mb-6'>
@@ -13,17 +39,23 @@ export const ConvertToNGSIv2Content: React.FC = () => {
         </select>
       </div>
 
-      <label
-        className='block mb-2 text-sm font-medium text-gray-900'
-        htmlFor='file_input'>
-        ファイルアップロード
-      </label>
-      <input
-        className='block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none'
-        aria-describedby='file_input_help'
-        id='file_input'
-        type='file'
-      />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label
+          className='block mb-2 text-sm font-medium text-gray-900'
+          htmlFor='file_input'>
+          ファイルアップロード
+        </label>
+        <input
+          className='block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none'
+          aria-describedby='file_input_help'
+          id='file_input'
+          type='file'
+          {...register("file")}
+        />
+        <button className='btn btn-sm mt-2' type='submit'>
+          変換
+        </button>
+      </form>
     </div>
   );
 };
