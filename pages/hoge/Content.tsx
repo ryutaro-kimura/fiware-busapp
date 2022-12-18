c976c8e87f91b2796edd1afa5d470db7ae9e0d9b
ryutaro-kimura <sasuker2264@gmail.com>
2022-12-19 07:42:05 +0900

    #6 add: 佐賀県バスのリアルタイムデータを取得

---
+++ pages/hoge/Content.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import { Props } from "./index.page";
+
+export const RealtimeBusDataContent: React.FC<Props> = props => {
+  const [isGet, setGet] = useState(false);
+
+  return (
+    <div>
+      {isGet ? (
+        <div>{props.FeedEntities}</div>
+      ) : (
+        <button onClick={() => setGet(true)}>表示する</button>
+      )}
+    </div>
+  );
+};
---
+++ pages/hoge/index.page.tsx
@@ -0,0 +1,56 @@
+import type { NextPage } from "next";
+import { RealtimeBusDataContent } from "./Content";
+import axios from "axios";
+import GtfsRealtimeBindings from "gtfs-realtime-bindings";
+import { GetServerSideProps } from "next";
+import request from "request";
+
+export type Props = {
+  FeedEntities: Object;
+};
+
+// type FeedEntity = {
+//   id: string
+//   vehicle: VehiclePosition {
+//     trip: [TripDescriptor],
+//     vehicle: [VehicleDescriptor],
+//     position: [Position],
+//     currentStopSequence: 3,
+//     timestamp: [Long]
+//   }
+// }
+
+const RealtimeBusDataPage: NextPage<Props> = props => {
+  return <RealtimeBusDataContent FeedEntities={props.FeedEntities} />;
+};
+
+export default RealtimeBusDataPage;
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const url = "http://opendata.sagabus.info/vehicle.pb";
+  const FeedEntities = await axios
+    .get(url, { responseType: "arraybuffer" })
+    .then(res => {
+      const vehicles = new Array<any>();
+      const feed = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(
+        new Uint8Array(res.data)
+      );
+      feed.entity.forEach((entity: any) => {
+        // entityの型はデフォのvehicleオブジェクト
+        vehicles.push(entity);
+      });
+      return vehicles;
+    })
+    .catch(error => {
+      console.log("error----", error);
+      return [];
+    });
+  console.log(FeedEntities[0].id);
+
+  const props: Props = {
+    FeedEntities: JSON.stringify(FeedEntities)
+  };
+  return { props: props };
+};
+
+// あとでこれ見て整理：https://www.sukerou.com/2022/02/nextjs-getserversideprops.html
