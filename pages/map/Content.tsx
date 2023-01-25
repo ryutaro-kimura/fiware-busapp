385987a0740a399b2c31159a0f86bd05beb89d37
ryutaro-kimura <sasuker2264@gmail.com>
2023-01-26 04:15:14 +0900

    prettier

--- pages/map/Content.tsx
+++ pages/map/Content.tsx
@@ -1,16 +1,14 @@
-import { useState } from "react";
-import { Props } from "./index.page";
+import { useState } from 'react'
+import GoogleMapComponent from './GoogleMap'
+import { Props } from './index.page'
 
 export const MapContent: React.FC<Props> = (props) => {
-  const [isGet, setGet] = useState(false);
+  const [isGet, setGet] = useState(false)
 
   return (
     <div>
-      {isGet ? (
-        <div>{props.FeedEntities}</div>
-      ) : (
-        <button onClick={() => setGet(true)}>表示する</button>
-      )}
+      <GoogleMapComponent />
+      {isGet ? <div>{props.FeedEntities}</div> : <button onClick={() => setGet(true)}>表示する</button>}
     </div>
-  );
-};
+  )
+}
--- pages/map/index.page.tsx
+++ pages/map/index.page.tsx
@@ -1,13 +1,13 @@
-import type { NextPage } from "next";
-import { MapContent } from "./Content";
-import axios from "axios";
-import GtfsRealtimeBindings from "gtfs-realtime-bindings";
-import { GetServerSideProps } from "next";
-import request from "request";
+import type { NextPage } from 'next'
+import { MapContent } from './Content'
+import axios from 'axios'
+import GtfsRealtimeBindings from 'gtfs-realtime-bindings'
+import { GetServerSideProps } from 'next'
+import request from 'request'
 
 export type Props = {
-  FeedEntities: Object;
-};
+  FeedEntities: Object
+}
 
 // type FeedEntity = {
 //   id: string
@@ -21,36 +21,34 @@ export type Props = {
 // }
 
 const MapPage: NextPage<Props> = (props) => {
-  return <MapContent FeedEntities={props.FeedEntities} />;
-};
+  return <MapContent FeedEntities={props.FeedEntities} />
+}
 
-export default MapPage;
+export default MapPage
 
 export const getServerSideProps: GetServerSideProps = async () => {
-  const url = "http://opendata.sagabus.info/vehicle.pb";
+  const url = 'http://opendata.sagabus.info/vehicle.pb'
   const FeedEntities = await axios
-    .get(url, { responseType: "arraybuffer" })
+    .get(url, { responseType: 'arraybuffer' })
     .then((res) => {
-      const vehicles = new Array<any>();
-      const feed = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(
-        new Uint8Array(res.data)
-      );
+      const vehicles = new Array<any>()
+      const feed = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(new Uint8Array(res.data))
       feed.entity.forEach((entity: any) => {
         // entityの型はデフォのvehicleオブジェクト
-        vehicles.push(entity);
-      });
-      return vehicles;
+        vehicles.push(entity)
+      })
+      return vehicles
     })
     .catch((error) => {
-      console.log("error----", error);
-      return [];
-    });
-  console.log(FeedEntities[0].id);
+      console.log('error----', error)
+      return []
+    })
+  // console.log(FeedEntities[0].id);
 
   const props: Props = {
-    FeedEntities: JSON.stringify(FeedEntities),
-  };
-  return { props: props };
-};
+    FeedEntities: JSON.stringify(FeedEntities)
+  }
+  return { props: props }
+}
 
 // あとでこれ見て整理：https://www.sukerou.com/2022/02/nextjs-getserversideprops.html
