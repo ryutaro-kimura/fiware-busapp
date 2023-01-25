d4ba1249d8c06a6f36befefe64e21df3644ba833
ryutaro-kimura <sasuker2264@gmail.com>
2023-01-26 04:15:31 +0900

    feat: ✨GoogleMap

--- pages/map/GoogleMap.tsx
+++ pages/map/GoogleMap.tsx
@@ -1,39 +1,34 @@
-import React, { useState } from "react";
-import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+import React, { useState } from 'react'
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api'
 
 // 佐賀市
 const center = {
   lat: 35.65224138251394,
-  lng: 139.70450901142888,
-};
+  lng: 139.70450901142888
+}
 
 const GoogleMapComponent: React.FC = () => {
-  const [pin, setPin] = useState();
+  const [pin, setPin] = useState()
   const containerStyle = {
-    height: "100vh",
-    width: "100%",
-  };
+    height: '100vh',
+    width: '100%'
+  }
 
   const setLatLng = (props: any) => {
     const pos: any = {
       lat: props.latLng.lat(),
-      lng: props.latLng.lng(),
-    };
-    setPin(pos);
-  };
+      lng: props.latLng.lng()
+    }
+    setPin(pos)
+  }
 
   return (
-    <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAP_API_KEY!}>
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={3}
-        onClick={setLatLng}
-      >
+    <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY!}>
+      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={3} onClick={setLatLng}>
         {pin && <Marker position={pin} />}
       </GoogleMap>
     </LoadScript>
-  );
-};
+  )
+}
 
-export default GoogleMapComponent;
+export default GoogleMapComponent
