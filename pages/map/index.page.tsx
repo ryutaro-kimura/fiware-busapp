fa10c9b91cbac85a35008b9a4d877b331a0e931d
ryutaro-kimura <sasuker2264@gmail.com>
2023-01-27 12:55:48 +0900

    feat: ✨fiwareからbusStopのデータを取得

--- pages/map/index.page.tsx
+++ pages/map/index.page.tsx
@@ -4,49 +4,23 @@ import axios from 'axios'
 import GtfsRealtimeBindings from 'gtfs-realtime-bindings'
 import { GetServerSideProps } from 'next'
 import request from 'request'
+import { getOrionData, NgsiBusStop } from '../../src/utils/api'
 
-export type Props = {
-  FeedEntities: Object
+type Props = {
+  busStops: NgsiBusStop[]
 }
 
-// type FeedEntity = {
-//   id: string
-//   vehicle: VehiclePosition {
-//     trip: [TripDescriptor],
-//     vehicle: [VehicleDescriptor],
-//     position: [Position],
-//     currentStopSequence: 3,
-//     timestamp: [Long]
-//   }
-// }
-
 const MapPage: NextPage<Props> = (props) => {
-  return <MapContent FeedEntities={props.FeedEntities} />
+  return <MapContent busStops={props.busStops} />
 }
 
 export default MapPage
 
 export const getServerSideProps: GetServerSideProps = async () => {
-  const url = 'http://opendata.sagabus.info/vehicle.pb'
-  const FeedEntities = await axios
-    .get(url, { responseType: 'arraybuffer' })
-    .then((res) => {
-      const vehicles = new Array<any>()
-      const feed = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(new Uint8Array(res.data))
-      feed.entity.forEach((entity: any) => {
-        // entityの型はデフォのvehicleオブジェクト
-        vehicles.push(entity)
-      })
-      return vehicles
-    })
-    .catch((error) => {
-      console.log('error----', error)
-      return []
-    })
-  // console.log(FeedEntities[0].id);
-
+  const gtfsBusStops = await getOrionData('GtfsStop')
+  console.log(gtfsBusStops)
   const props: Props = {
-    FeedEntities: JSON.stringify(FeedEntities)
+    busStops: gtfsBusStops
   }
   return { props: props }
 }
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from 'next'
 // import { BASE_API_URL } from "../../secrets";
 import AmazonCognitoIdentity, {
   AuthenticationDetails,
@@ -7,59 +7,86 @@ import AmazonCognitoIdentity, {
   CognitoUserPool,
   IAuthenticationDetailsData,
   ICognitoUserPoolData
-} from "amazon-cognito-identity-js";
+} from 'amazon-cognito-identity-js'
 
 const authenticationData: IAuthenticationDetailsData = {
   Username: process.env.NEXT_PUBLIC_USERNAME!,
   Password: process.env.NEXT_PUBLIC_PASSWORD
-};
-const authenticationDetails = new AuthenticationDetails(authenticationData);
+}
+const authenticationDetails = new AuthenticationDetails(authenticationData)
 const poolData: ICognitoUserPoolData = {
   UserPoolId: process.env.NEXT_PUBLIC_USER_POOL_ID!,
   ClientId: process.env.NEXT_PUBLIC_APP_CLIENT_ID!
-};
-const userPool = new CognitoUserPool(poolData);
+}
+const userPool = new CognitoUserPool(poolData)
 const userData = {
   Username: authenticationData.Username,
   Pool: userPool
-};
-const cognitoUser = new CognitoUser(userData);
+}
+const cognitoUser = new CognitoUser(userData)
 
-export const getOrionData = async () => {
-  cognitoUser.authenticateUser(authenticationDetails, {
-    onSuccess: async function (result) {
-      const accessToken = result.getAccessToken().getJwtToken();
+// export type NgsiBusStop = {
+//   id: string
+//   type: string
+//   code: string
+//   name: string
+//   location: {
+//     type: string
+//     corrdinates: number[]
+//   }
+//   operateBy: string[]
+// }
 
-      const idToken = result.getIdToken().getJwtToken();
-      console.log(`TOKEN: ${idToken}`);
+export type NgsiBusStop = {
+  id: string
+  type: string
+  code: { type: string; value: string; metadata: {} }
+  location: {
+    type: string
+    value: Object
+    corrdinates: number[]
+  }
+  name: { type: string; value: number; metadata: {} }
+  operateBy: string[]
+}
 
-      const resp = await fetch(
-        `${process.env.NEXT_PUBLIC_ORION_ENDPOINT}/v2/entities?type=Room`,
-        {
-          headers: {
-            Authorization: idToken
+export const getOrionData = (gtfsModelType: string) => {
+  return new Promise((resolve, reject) => {
+    cognitoUser.authenticateUser(authenticationDetails, {
+      onSuccess: async function (result) {
+        const accessToken = result.getAccessToken().getJwtToken()
+
+        const idToken = result.getIdToken().getJwtToken()
+        console.log(`TOKEN: ${idToken}`)
+
+        const resp = await fetch(
+          `${process.env.NEXT_PUBLIC_ORION_ENDPOINT}/v2/entities?type=${gtfsModelType}&options=keyValues`,
+          {
+            headers: {
+              Authorization: idToken
+            }
           }
-        }
-      );
-      const body = await resp.json();
-      console.log(body);
-      return body;
-    },
+        )
+        const body = await resp.json()
+        resolve(body)
+      },
 
-    onFailure: function (err) {
-      console.log(`Got Error =======`);
-      console.log(err);
-    }
-  });
-};
+      onFailure: function (err) {
+        console.log(`Got Error =======`)
+        console.log(err)
+        reject(err)
+      }
+    })
+  })
+}
 
 export const getGithubId = async () => {
   // 環境変数の確認用
   // console.log(authenticationData);
 
-  const url = "https://api.github.com/users/ryutaro-kimura";
-  const data = await fetch(url);
-  const json = await data.json();
-  const githubId: number = json.login;
-  return await githubId;
-};
+  const url = 'https://api.github.com/users/ryutaro-kimura'
+  const data = await fetch(url)
+  const json = await data.json()
+  const githubId: number = json.login
+  return await githubId
+}
